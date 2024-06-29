c921d1c3128e23efd903df372a7e5d5abb884e96
Tonthowi Al Ahyar <tonthowi@gmail.com>
2024-06-29 07:32:45 +0700

    Start styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,23 @@ import './App.css'
 
 const Display = ({result, price, discount}) => {
   return (
-    <div className='mb-8'>
+    <div className='flex flex-col mb-8'>
       <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
         {result}
       </h2>
-      <p>{price}</p>
-      <p>{discount}</p>
+      <div className='flex flex-row'>
+        <p className='flex-auto'>{price}</p>
+        <p className='flex-auto'>{discount}</p>
+      </div>
     </div>
   )
 }
 
 const Button = ({handleClick, text}) => {
   return (
-    <div className='mt-8'>
+    <div className='w-full mt-8'>
       <button
-        className="w-full bg-slate-900 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-slate-900 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
         onClick={handleClick}
         >
         {text}
@@ -60,12 +62,14 @@ function App() {
   }
 
   return (
-    <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div className="flex flex-col mx-auto max-w-xl sm:px-6 lg:px-8">
+      <div className='bg-slate-200'>
       <Display
         result={new Intl.NumberFormat('id-ID').format(result)}
         price={new Intl.NumberFormat('id-ID').format(price)}
         discount={discount}
       />
+      </div>
       <Input handleChange = {handlePriceChange} placeholder={"Input price"} />
       <Input handleChange = {handleDiscountChange} placeholder ={"Input discount"} />
       <Button handleClick={calculateDiscount} text={"Calculate"} />
